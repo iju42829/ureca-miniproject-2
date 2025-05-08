bea7aae03565ae945b78bf32381689cd317c0aa6
iju42829 <ijud4282@gmail.com>
2025-05-08 16:43:29 +0900

    feat: 공통 API 응답 클래스 추가

---
+++ src/main/java/com/ureca/miniproject/common/BaseCode.java
@@ -0,0 +1,17 @@
+package com.ureca.miniproject.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum BaseCode {
+    // USER
+    USER_CREATE_SUCCESS("CREATE_USER_201", HttpStatus.CREATED, "회원 정보가 성공적으로 등록되었습니다."),
+    USER_NOT_FOUND("NOT_FOUND_USER_404", HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다.");
+
+    private final String code;
+    private final HttpStatus status;
+    private final String message;
+}

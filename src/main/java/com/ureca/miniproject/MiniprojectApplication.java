ff0995407de1db802db4f9bca5b2bcb6d3ec5d74
son <cliff615@naver.com>
2025-05-08 17:21:22 +0900

    config제거

--- src/main/java/com/ureca/miniproject/MiniprojectApplication.java
+++ src/main/java/com/ureca/miniproject/MiniprojectApplication.java
@@ -8,7 +8,6 @@ public class MiniprojectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MiniprojectApplication.class, args);
-        System.out.println("");
     }
 
 }

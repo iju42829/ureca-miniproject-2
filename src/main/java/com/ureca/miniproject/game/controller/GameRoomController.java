cf48d4426e928202c7ea738fa68e1cb9dbf8c48d
JaeYun <116072376+iju42829@users.noreply.github.com>
2025-05-09 13:25:28 +0900

    로그인된 사용자 게임방 생성 기능 구현 (#12)

--- src/main/java/com/ureca/miniproject/game/controller/GameRoomController.java
+++ src/main/java/com/ureca/miniproject/game/controller/GameRoomController.java
@@ -1,11 +1,14 @@
 package com.ureca.miniproject.game.controller;
 
 import com.ureca.miniproject.common.ApiResponse;
+import com.ureca.miniproject.config.MyUserDetails;
 import com.ureca.miniproject.game.controller.request.CreateRoomRequest;
 import com.ureca.miniproject.game.service.GameRoomService;
 import com.ureca.miniproject.game.service.response.CreateGameRoomResponse;
+import com.ureca.miniproject.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import static com.ureca.miniproject.common.BaseCode.*;
@@ -19,9 +22,9 @@ public class GameRoomController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<CreateGameRoomResponse>> createGameRoom(@RequestBody CreateRoomRequest createRoomRequest,
-                                                                              @RequestParam Long userId) {
+                                                                              @AuthenticationPrincipal MyUserDetails myUserDetails) {
 
-        CreateGameRoomResponse createGameRoomResponse = gameRoomService.createGameRoom(createRoomRequest, userId);
+        CreateGameRoomResponse createGameRoomResponse = gameRoomService.createGameRoom(createRoomRequest, myUserDetails);
 
         return ResponseEntity.ok(ApiResponse
                 .of(GAME_ROOM_CREATE_SUCCESS, createGameRoomResponse));

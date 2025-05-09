96f119163045f7837b2eaeaceec22e0eb2bba8f8
iju42829 <ijud4282@gmail.com>
2025-05-09 13:21:00 +0900

    feat: 로그인된 사용자 게임방 생성 기능 구현

--- src/main/java/com/ureca/miniproject/game/service/GameRoomService.java
+++ src/main/java/com/ureca/miniproject/game/service/GameRoomService.java
@@ -1,9 +1,10 @@
 package com.ureca.miniproject.game.service;
 
+import com.ureca.miniproject.config.MyUserDetails;
 import com.ureca.miniproject.game.controller.request.CreateRoomRequest;
 import com.ureca.miniproject.game.service.response.CreateGameRoomResponse;
 
 public interface GameRoomService {
 
-    CreateGameRoomResponse createGameRoom(CreateRoomRequest createRoomRequest, Long userId);
+    CreateGameRoomResponse createGameRoom(CreateRoomRequest createRoomRequest, MyUserDetails myUserDetails);
 }

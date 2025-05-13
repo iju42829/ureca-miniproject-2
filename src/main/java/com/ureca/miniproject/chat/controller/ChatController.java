f0659839a5b5366a9a39784e286e04aa1930304f
Son Minhyeok <50442066+Sonminhyeok@users.noreply.github.com>
2025-05-13 11:07:31 +0900

    feat:game manager/timer (#34)

--- src/main/java/com/ureca/miniproject/chat/controller/ChatController.java
+++ src/main/java/com/ureca/miniproject/chat/controller/ChatController.java
@@ -9,8 +9,7 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.ureca.miniproject.chat.dto.ChatMessage;
-import com.ureca.miniproject.chat.dto.VoteResultDto;
-import com.ureca.miniproject.chat.service.VoteService;
+import com.ureca.miniproject.chat.service.StateManager;
 import com.ureca.miniproject.chat.tool.ChatRoomUserTool;
 
 import lombok.RequiredArgsConstructor;
@@ -19,40 +18,37 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class ChatController {
 
-
     private final ChatRoomUserTool userRepo;
+    private final StateManager stateManager;
 
     @MessageMapping("/chat.send/{roomId}")
     @SendTo("/topic/chat/{roomId}")
     public ChatMessage send(@Payload ChatMessage message, @DestinationVariable("roomId") String roomId, Principal principal) {
-    	
-    	String username = principal.getName();
-    	System.out.println(message+roomId+username+"/send");
-    	message.setSender(username);
-        if (message.getType() == ChatMessage.MessageType.ENTER) {
-            userRepo.addUser(roomId, message.getSender());
-            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-            System.out.println(message.getSender()+"입장");
-        } 
-        else if (message.getType() == ChatMessage.MessageType.LEAVE) {
-            userRepo.removeUser(roomId, message.getSender());
-            message.setMessage(message.getSender() + "님이 퇴장하셨습니다.");
-            System.out.println(message.getSender()+"퇴장");
-        }
-        else if (message.getType() == ChatMessage.MessageType.END_TIME) {
-            message.setMessage("시간이 종료되었습니다.");
-            message.setStartTime(System.currentTimeMillis());
-        }
-        else if (message.getType() == ChatMessage.MessageType.START_TIME) {
-            message.setMessage("토론을 시작합니다.");
-            message.setStartTime(System.currentTimeMillis()); 
-        }
 
+        String username = principal.getName();
+        message.setSender(username);
+
+        switch (message.getType()) {
+            case ENTER -> {
+                userRepo.addUser(roomId, username);
+                message.setMessage(username + "님이 입장하셨습니다.");
+            }
+            case LEAVE -> {
+                userRepo.removeUser(roomId, username);
+                message.setMessage(username + "님이 퇴장하셨습니다.");
+            }
+            case START_TIME -> {
+                message.setParticipants(userRepo.getUsers(roomId));
+                stateManager.startDebate(roomId, message);
+                return null;
+            }
+            case END_TIME -> {
+                stateManager.endDebate(roomId, message);
+                return null;
+            }
+        }
 
         message.setParticipants(userRepo.getUsers(roomId));
         return message;
     }
-    
-
-
 }

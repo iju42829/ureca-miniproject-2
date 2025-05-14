f204830af000c5e01a68a0fbe17eebd477a70259
JaeYun <116072376+iju42829@users.noreply.github.com>
2025-05-14 14:40:34 +0900

    게임 서비스 레이어 테스트 추가 (#49)

--- src/main/java/com/ureca/miniproject/game/controller/request/EndGameRequest.java
+++ src/main/java/com/ureca/miniproject/game/controller/request/EndGameRequest.java
@@ -1,11 +1,15 @@
 package com.ureca.miniproject.game.controller.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Getter @Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class EndGameRequest {
     private List<String> winners;
 }
